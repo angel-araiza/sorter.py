d75dab0549a370bb118c721c9461db1823fe973e
angel <angeldrummer2000@gmail.com>
2024-09-25 15:51:00 -0700

    list set

--- sorter.py
+++ sorter.py
@@ -5,8 +5,13 @@ file_path = "denver_data.xlsx"
 df = pd.read_excel(file_path, engine ='openpyxl')
 
 mint_profile_id = df['Profile MINT ID']
+list = ""
+
 
 for index, value in mint_profile_id.items():
   if pd.notna(value):
-    print(f"Row {index}: {value}")
+    list += str(int(value)) + ", "
+
+new_list = list[:-2]
+print(new_list)
 
