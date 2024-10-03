f08b8aa388e3a9e3692a6fc10d5c5e9f3d65225b
angel <angeldrummer2000@gmail.com>
2024-10-03 11:21:31 -0700

    v1 - b4 demo

--- grapher.py
+++ grapher.py
@@ -20,30 +20,27 @@ month_counts = df['STARTMONTH'].value_counts().reset_index()
 month_counts.columns = ['STARTMONTH', 'Count']
 
 
-# Bar chart with category counts
+# Bar chart with category counts - Cities met in
 plt.figure(figsize=(10,6))
 plt.bar(category_counts['CityMetIn'].head(15), category_counts['Count'].head(15), color='skyblue')
 plt.xlabel('Meeting Location')
 plt.ylabel('Count')
-plt.title('Count of top 15 Cities that have a meeting date from 2023 and on.')
+plt.title('Top 15 Cities Where Lost Leads Held Meetings (2023 Onwards)')
 plt.xticks(rotation=45)
 plt.tight_layout()
-# plt.show()
 
 
-# Could have a graph that shows the time of year that these lost meetings tend to happen
-# -- If so, I would need to collect Start Month and Day to do this off of. Maybe could do off of month
-# Could ask chat for ideas on how to display this data
+#Bar chart for month distrubution of lost leads
+months = ['January', 'February', 'March', 'April', 'May', 'June', 
+          'July', 'August', 'September', 'October', 'November', 'December']
 
-#Bar chart
 plt.figure(figsize=(10,6))
 plt.bar(month_counts['STARTMONTH'], month_counts['Count'], color='green')
 plt.xlabel('Months')
 plt.ylabel('Count')
-plt.title('Month of Lost leads')
-plt.xticks(rotation=45)
+plt.title('Monthly Distribution of Lost Leads (2023 Onwards)')
+plt.xticks(ticks=range(1, 13), labels=months, rotation=45)
 plt.tight_layout()
-
 
 # Pie Chart
 plt.figure(figsize=(14,8))
@@ -53,7 +50,7 @@ plt.pie(ms_counts['Count'].head(10),
           startangle=140,
           labeldistance=1.1,
           textprops={'fontsize':8})
-plt.title('Pie Chart of Market Segments')
+plt.title('Market Segment Frequencies of Lost Leads (2023 Onwards)')
 plt.tight_layout()
 
 
@@ -64,10 +61,12 @@ plt.figure(figsize=(10,6))
 plt.hist(df['PEAKRMSRES'], bins=12, color='purple', edgecolor='black')
 plt.xlabel('Peak Rooms Reserved')
 plt.ylabel('Amount')
-plt.title('The Peak Rooms in lost leads')
+plt.title('Lost Leads Grouped by Peak Rooms Reserved (2023 Onward)')
 plt.tight_layout()
 plt.show()
 
+"""
+How could I get the thousand comma added to my x label?"""
 
 
 
